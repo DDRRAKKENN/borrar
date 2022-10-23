36bc663e837368ba2f2ae114e043043a642fa40d
jorge ignacio lara ceballos <jorgelaraceballos@gmail.com>
2022-10-23 01:31:46 -0300

    cambio ed cosas aca dentro

--- algo.py
+++ algo.py
@@ -2,6 +2,7 @@
 
 
 from logging import debug
+from socket import CAN_BCM_RX_TIMEOUT
 
 
 def sumames():
@@ -16,3 +17,5 @@ def sumames():
 
 fechas = [20200130,20201114,20220930,20221221]
 debug
+
+esto tambien CAN_BCM_RX_TIMEOUTS
