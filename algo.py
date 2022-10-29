867f28cc35a1e15e2815ecef9624c2aadf1e8b56
jorge ignacio lara ceballos <jorgelaraceballos@gmail.com>
2022-10-29 01:01:46 -0300

    agrewgado de cosas locas

--- algo.py
+++ algo.py
@@ -1,1 +1,12 @@
-mensaje desde la otra pc jejeje
+print("ESTO ES UNA LINEA")
+print("ESTO ES UNA LINEA SEGUNDA")
+print("ESTO ES UNA LINEA TERCERA")
+
+
+x = 2
+
+while x < 6:
+    x+=1
+    print("esto entro aca")
+else:
+    print("no hizo nada")
